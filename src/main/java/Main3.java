18f39419135bbfbd786e0562a46fd1c3191cb793
vikram <vikram.bedi97@gmail.com>
2021-05-05 18:06:14 +0530

    Seperate class

---
+++ src/main/java/Main3.java
@@ -0,0 +1,63 @@
+import com.amazon.sqs.javamessaging.ProviderConfiguration;
+import com.amazon.sqs.javamessaging.SQSConnection;
+import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+
+import javax.jms.*;
+
+public class Main3 {
+    public static void main(String[] args) throws JMSException {
+
+        SQSListener sqsListener=new SQSListener("vbedi-sqs-queue",Regions.US_WEST_2);
+        sqsListener.makeConnection().start();
+
+    }
+};
+
+class SQSListener{
+
+    private String queueName;
+    private Regions region;
+
+    public SQSListener(String queueName, Regions region) {
+        this.queueName = queueName;
+        this.region = region;
+    }
+
+    private SQSConnectionFactory sqsConnectionFactory(){
+        return new SQSConnectionFactory(
+                new ProviderConfiguration(),
+                AmazonSQSClientBuilder.standard().withRegion(this.region)
+        );
+    }
+
+    private SQSConnection connection() throws JMSException {
+        return sqsConnectionFactory().createConnection();
+    }
+
+    public SQSConnection makeConnection() throws JMSException {
+        SQSConnection sqsConnection=connection();
+        Session session=sqsConnection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+        Queue queue=session.createQueue(this.queueName);
+        MessageConsumer consumer=session.createConsumer(queue);
+        consumer.setMessageListener(new MyListener2());
+        return sqsConnection;
+    }
+
+}
+
+
+class MyListener2 implements MessageListener {
+    @Override
+    public void onMessage(Message message) {
+        try{
+            String text = ((TextMessage) message).getText();
+            System.out.println(text);
+
+        }catch(JMSException e){
+            e.printStackTrace();
+        }
+    }
+}
